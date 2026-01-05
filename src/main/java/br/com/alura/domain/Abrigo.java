68b74833a35868433b2027a5dd38555d586d323c
matheus-vsm <scompinha@hotmail.com>
2026-01-04 21:39:53 -0300

    aula 3.5

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    private long id;
     private String nome;
     private String telefone;
     private String email;
@@ -12,4 +13,35 @@ public class Abrigo {
         this.email = email;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
